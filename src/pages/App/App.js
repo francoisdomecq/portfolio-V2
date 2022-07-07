41751439969bb759244550e3b90811201c5adafa
francoisdomecq <fradomecq@gmail.com>
2022-07-07 14:53:55 +0200

    recenter div on click

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -10,16 +10,16 @@ import './App.css';
 function App() {
     function reveal() {
         var reveals = document.querySelectorAll('.reveal-on-scroll');
-        console.log(reveals);
         for (var i = 0; i < reveals.length; i++) {
             var windowHeight = window.innerHeight;
             var elementTop = reveals[i].getBoundingClientRect().top;
             var elementVisible = 125;
             if (elementTop < windowHeight - elementVisible) {
                 reveals[i].classList.add('active-on-scroll');
-            } else {
-                reveals[i].classList.remove('active-on-scroll');
             }
+            /*else {
+                reveals[i].classList.remove('active-on-scroll');
+            } */
         }
     }
 
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -16,9 +16,18 @@ function Projects() {
 
     async function selectProject(project) {
         await setSelectedProject(project);
-        document
-            .getElementById('SelectedProject')
-            .classList.add('reveal-selected-project');
+        const element = document.getElementById('SelectedProject');
+        element.classList.add('reveal-selected-project');
+        const height = element.offsetTop;
+        window.scrollTo(0, height - 10);
+    }
+
+    async function resetProject() {
+        await setSelectedProject(null);
+        const element = document.getElementsByClassName('reveal-on-scroll');
+        for (let i = 0; i < element.length; i++) {
+            element[i].classList.toggle('active-on-scroll');
+        }
     }
 
     useEffect(() => {
@@ -27,20 +36,25 @@ function Projects() {
     }, []);
 
     return selectedProject ? (
-        <section className="page-project " id="SelectedProject">
-            <button onClick={() => setSelectedProject(null)}>Reset</button>
-            <p style={{ color: 'white' }}>{selectedProject.name}</p>
+        <section className="page-projects">
+            <section className="page-project " id="SelectedProject">
+                <div className="page-project-back-button">
+                    <span onClick={() => resetProject()}>&#8249; Go back</span>
+                </div>
+
+                <p style={{ color: 'white' }}>{selectedProject.name}</p>
+            </section>
         </section>
     ) : (
         <section className="page-projects" id="projects">
             <h1 className="title-projects reveal-on-scroll">My projects</h1>
+
             <Filter
                 selectedFilter={selectedFilter}
                 setSelectedFilter={setSelectedFilter}
                 projects={projects}
                 setFilteredProjects={setFilteredProjects}
             />
-
             <div className="container-projects reveal-on-scroll">
                 <AnimatePresence>
                     {filteredProjects.map((project) => {
