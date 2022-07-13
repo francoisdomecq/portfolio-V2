787015ae980afa99ced766881f08edb1354d0cf6
francoisdomecq <fradomecq@gmail.com>
2022-07-13 10:34:28 +0200

    skills page

---
+++ src/data/skillsData.js
@@ -0,0 +1,37 @@
+import { siTypescript } from 'simple-icons/icons';
+import { siJavascript } from 'simple-icons/icons';
+import { siGit } from 'simple-icons/icons';
+import { siReact } from 'simple-icons/icons';
+import { siNodedotjs } from 'simple-icons/icons';
+import { siMongodb } from 'simple-icons/icons';
+import { siAzuredevops } from 'simple-icons/icons';
+import { siAdobexd } from 'simple-icons/icons';
+import { siFigma } from 'simple-icons/icons';
+import { siCsharp } from 'simple-icons/icons';
+import { siCss3 } from 'simple-icons/icons';
+import { siExpress } from 'simple-icons/icons';
+import { siHtml5 } from 'simple-icons/icons';
+import { siPython } from 'simple-icons/icons';
+import { siDotnet } from 'simple-icons/icons';
+import { siKeras } from 'simple-icons/icons';
+import { siJest } from 'simple-icons/icons';
+
+export const skillsData = [
+    siTypescript,
+    siJavascript,
+    siGit,
+    siReact,
+    siNodedotjs,
+    siMongodb,
+    siAzuredevops,
+    siAdobexd,
+    siFigma,
+    siCsharp,
+    siCss3,
+    siExpress,
+    siHtml5,
+    siPython,
+    siDotnet,
+    siKeras,
+    siJest,
+];
--- src/pages/Skills/Skills.js
+++ src/pages/Skills/Skills.js
@@ -1,60 +1,115 @@
 import { useEffect, useState } from 'react';
+import { Cloud, renderSimpleIcon } from 'react-icon-cloud';
 
-import SkillCard from '../../components/Skills_SkillCard/SkillCard';
-import Filter from '../../components/Skills_Filter/Filter';
 import './Skills.css';
-
-import { technologiesData } from '../../data/technologies';
-import { AnimatePresence } from 'framer-motion';
-/*
-Software : 
-C#, python, git, 
-
-Frontend :
-html, css, js, ts, bootstrap, greensock, git, react, react native,
-
-Bakcend : 
-js, ts, nodejs, express, mongodb, c#, asp.net core, azure, sql, nosql, git, entity framework
-
-Ux design : 
-Figma, adobeXd, Axure, user tests,  
-*/
+import { skillsData } from '../../data/skillsData';
 
 function Skills() {
-    const [selectedFilter, setSelectedFilter] = useState('');
-    const [technologies, setTechnologies] = useState([]);
-    const [filteredTechnologies, setFilterTechnologies] = useState([]);
+    const [selectedIcon, setSelectedIcon] = useState('');
 
-    useEffect(() => {
-        setTechnologies(technologiesData);
-        setFilterTechnologies(technologiesData);
-    }, []);
+    function onClickIcon(e, icon) {
+        e.preventDefault();
+    }
+
+    function displayIconName(icon) {
+        console.log(icon.title);
+    }
+
+    const renderCustomIcon = (icon, bg) => {
+        return renderSimpleIcon({
+            icon,
+            size: 64,
+            bgHex: bg,
+            aProps: {
+                href: undefined,
+                target: undefined,
+                rel: undefined,
+                onClick: (e) => onClickIcon(e, icon),
+                onMouseOver: () => displayIconName(icon),
+            },
+        });
+    };
+
+    const colour = '#08fdd8';
+    const cloudIcons = skillsData.map((i) => renderCustomIcon(i, colour));
 
     return (
         <div className="skills-section" id="skills">
             <h1 className="title-skills reveal-on-scroll">My skills</h1>
-            <Filter
-                selectedFilter={selectedFilter}
-                setSelectedFilter={setSelectedFilter}
-                technologies={technologies}
-                setFilteredTechnologies={setFilterTechnologies}
-            />
-            <div className="skills-container reveal-on-scroll">
-                <AnimatePresence>
-                    {filteredTechnologies.map((technology) => {
-                        return (
-                            <SkillCard
-                                key={technology.name}
-                                name={technology.name}
-                                img={technology.img}
-                                level={technology.level}
-                                info={technology.info}
-                            />
-                        );
-                    })}
-                </AnimatePresence>
+            <div className="skills-container">
+                <div className="skills-text">
+                    <p>
+                        Pariatur fugiat veniam nisi incididunt enim laboris
+                        ipsum esse cupidatat laboris quis officia
+                        aliqua.Excepteur deserunt sint elit aute proident ut
+                        labore laboris nulla dolore officia nostrud occaecat.
+                    </p>
+                    <p>
+                        Pariatur fugiat veniam nisi incididunt enim laboris
+                        ipsum esse cupidatat laboris quis officia
+                        aliqua.Excepteur deserunt sint elit aute proident ut
+                        labore laboris nulla dolore officia nostrud occaecat.
+                    </p>
+                    <p>
+                        Pariatur fugiat veniam nisi incididunt enim laboris
+                        ipsum esse cupidatat laboris quis officia
+                        aliqua.Excepteur deserunt sint elit aute proident ut
+                        labore laboris nulla dolore officia nostrud occaecat.
+                    </p>
+                </div>
+                <div className="cloud-container">
+                    <Cloud
+                        id="skills-canvas"
+                        canvasProps={{
+                            display: 'flex',
+                        }}
+                        className="skills-canvas reveal-on-scroll"
+                        options={{
+                            outlineColour: 'red',
+                            clickToFront: 500,
+
+                            fadeIn: 200,
+                            imadeMode: 'both',
+                            pinchZoom: true,
+                            shuffleTags: true,
+                            weight: true,
+                            zoom: 1.1,
+                            zoomMax: 1.5,
+                            zoomMin: 1,
+                            tooltip: 'native',
+                            tooltipDelay: 0,
+                            freezeActive: true,
+                        }}
+                    >
+                        {cloudIcons}
+                    </Cloud>
+                </div>
             </div>
         </div>
+        // <div className="skills-section" id="skills">
+        //     <h1 className="title-skills reveal-on-scroll">My skills</h1>
+        //     <Filter
+        //         selectedFilter={selectedFilter}
+        //         setSelectedFilter={setSelectedFilter}
+        //         technologies={technologies}
+        //         setFilteredTechnologies={setFilterTechnologies}
+        //     />
+        //     <div className="skills-container reveal-on-scroll">
+        //         <AnimatePresence>
+        //             {filteredTechnologies.map((technology) => {
+        //                 return (
+        //                     <SkillCard
+        //                         key={technology.name}
+        //                         name={technology.name}
+        //                         img={technology.img}
+        //                         level={technology.level}
+        //                         info={technology.info}
+        //                     />
+        //                 );
+        //             })}
+        //         </AnimatePresence>
+        //     </div>
+        // </div>
     );
 }
 
